b8ee7257024259db720bbc72cdc18032847c43d3
Michael <michael.sergeew@gmail.com>
2019-10-20 21:53:19 +0400

    финальная версия

---
+++ focusStart/Car.swift
@@ -0,0 +1,30 @@
+//
+//  Car.swift
+//  ar_book
+//
+//  Created by Mikhail Sergeev on 14.10.2019.
+//  Copyright © 2019 Михаил Сергеев. All rights reserved.
+//
+
+//import Foundation
+//
+//class Car {
+//   
+////    enum BodyType:String {
+////        case sedan = "sedan"
+////        case hatchback = "hatchback"
+////        case universal = "universal"
+////        case coupe = "coupe"
+////    }
+//    
+//    var yearOfManufacture:String
+//    var manufacturer:String
+//    var model:String
+//    var bodyType:String
+//    init(manufacturer:String, model:String, yearOfManufacture:String, bodyType:String  ) {
+//        self.manufacturer=manufacturer
+//        self.model=model
+//        self.yearOfManufacture=yearOfManufacture
+//        self.bodyType=bodyType
+//    }
+//}
